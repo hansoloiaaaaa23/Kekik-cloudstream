3780fafecf4ebb658d12c0164757d4616b02821d
hansoloiaaaaa23 <ey.u36e.mir@gmail.com>
2024-11-30 00:49:21 +0300

    Update bakalim.py

--- DiziBox/src/main/kotlin/com/keyiflerolsun/bakalim.py
+++ DiziBox/src/main/kotlin/com/keyiflerolsun/bakalim.py
@@ -7,7 +7,7 @@ from re              import search
 from Kekik.Sifreleme import CryptoJS
 
 oturum = CloudScraper()
-istek  = oturum.get("https://www.dizibox.de/hello-tomorrow-1-sezon-1-bolum-izle/")
+istek  = oturum.get("https://yabancidizi.tv/dizi/dandadan/sezon-1/bolum-1")
 secici = Selector(istek.text)
 
 iframe = secici.css("div#video-area iframe::attr(src)").get()
@@ -18,12 +18,12 @@ iframe = iframe.replace("king.php?v=", "king.php?wmode=opaque&v=")
 #     "isTrustedUser" : "true",
 #     "dbxu"          : "1722403730363"
 # })
-oturum.headers.update({"Referer": "https://www.dizibox.de/hello-tomorrow-1-sezon-1-bolum-izle/"})
+oturum.headers.update({"Referer": "https://yabancidizi.tv/dizi/dandadan/sezon-1/bolum-1"})
 istek  = oturum.get(iframe)
 secici = Selector(istek.text)
 iframe = secici.css("div#Player iframe::attr(src)").get()
 
-oturum.headers.update({"Referer": "https://www.dizibox.de/"})
+oturum.headers.update({"Referer": "https://yabancidizi.tv/"})
 istek     = oturum.get(iframe)
 cryptData = search(r"CryptoJS\.AES\.decrypt\(\"(.*)\",\"", istek.text).group(1)
 cryptPass = search(r"\",\"(.*)\"\);", istek.text).group(1)
